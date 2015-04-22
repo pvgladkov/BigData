6f6e21828b58b42ee53c9207aba4d79ae1e8b60b
Pavel Gladkov <webgeist.xor@gmail.com>
2015-04-23 00:39:51 +0300

    l5

--- lab_5/run.py
+++ lab_5/run.py
@@ -6,6 +6,8 @@ __author__ = 'Pavel Gladkov'
 import logging
 import json
 from vk_lib import Vk
+from collections import Counter
+from datetime import date, timedelta
 
 if __name__ == '__main__':
 
@@ -20,14 +22,68 @@ if __name__ == '__main__':
 
     result = {'gender': {}, 'age': {}, 'top_interest': ''}
 
-    vk_api = Vk()
-    members = vk_api.get_members(GROUP_ID)
-    logger.info('count: %s' % members.get('count', 0))
+    r_gender = {'male': 0, 'female': 0, '?': 0}
+    r_age = {"<=10": 0, "11-20": 0, "21-30": 0, "?": 0, ">=31": 0}
+    r_interests = Counter()
 
+    vk_api = Vk()
+
+    # пол
+    members = vk_api.get_members(GROUP_ID, 'sex')
+    logger.info('count: %s' % members.get('count', 0))
     member_list = members.get('items', [])
+    for member in member_list:
+        gender = int(member.get('sex', 0))
+        if gender == 1:
+            r_gender['female'] += 1
+        if gender == 2:
+            r_gender['male'] += 1
+        else:
+            r_gender['?'] += 1
+
+    end_date = date(2015, 4, 1)
+
+    def get_age_group(_date):
+        if _date is None:
+            return "?"
+        try:
+            day, month, year = _date.split('.')
+            start_date = date(int(year), int(month), int(day))
+        except ValueError:
+            return "?"
+        delta = end_date - start_date
+        years = delta.days / 365
+        if years <= 10:
+            return '<=10'
+        if 10 < years <= 20:
+            return '11-20'
+        if 20 < years <= 30:
+            return '21-30'
+        if years >= 30:
+            return '>=31'
+
+        return "?"
+
+    # возраст
+    members = vk_api.get_members(GROUP_ID, 'bdate')
+    logger.info('count: %s' % members.get('count', 0))
+    member_list = members.get('items', [])
+    for member in member_list:
+        r_age[get_age_group(member.get('bdate'))] += 1
+
+    # интересы
+    members = vk_api.get_members(GROUP_ID, 'interests')
+    logger.info('count: %s' % members.get('count', 0))
+    member_list = members.get('items', [])
+    for member in member_list:
+        r_age[get_age_group(member.get('bdate'))] += 1
 
     # результат
     with open('lab5statistics.json', 'w') as f:
+        result['top_interest'] = r_interests.most_common()
+        result['gender'] = r_gender
+        result['age'] = r_age
+
         f.write(json.dumps(result))
         f.close()
 
--- lab_5/vk_lib.py
+++ lab_5/vk_lib.py
@@ -10,8 +10,8 @@ class Vk(object):
     def __init__(self):
         self._url = 'http://ec2-52-17-77-210.eu-west-1.compute.amazonaws.com/method/'
 
-    def get_members(self, group_id):
+    def get_members(self, group_id, field):
         method = 'groups.getMembers'
-        _fields = ['interests']
+        _fields = [field]
         r = requests.get(self._url + method, params={'group_id': group_id, 'fields': _fields})
         return r.json().get('response', {})
