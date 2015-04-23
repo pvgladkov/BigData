49e79989d0191fab9440257a58d7d1f67ab87d63
Pavel Gladkov <p.gladkov@vectordigital.ru>
2015-04-23 10:40:29 +0300

    lab 5

--- lab_5/run.py
+++ lab_5/run.py
@@ -27,23 +27,6 @@ if __name__ == '__main__':
     r_age = {"<=10": 0, "11-20": 0, "21-30": 0, "?": 0, ">=31": 0}
     r_interests = []
 
-    vk_api = Vk()
-
-    # пол
-    members = vk_api.get_members(GROUP_ID, 'sex')
-    logger.info('count: %s' % members.get('count', 0))
-    member_list = members.get('items', [])
-    for member in member_list:
-        gender = int(member.get('sex', 0))
-        if gender == 1:
-            r_gender['female'] += 1
-        if gender == 2:
-            r_gender['male'] += 1
-        if gender == 0:
-            r_gender['?'] += 1
-
-    end_date = date(2015, 4, 1)
-
     def get_age_group(_date):
         if _date is None:
             return "?"
@@ -65,25 +48,42 @@ if __name__ == '__main__':
 
         return "?"
 
-    # возраст
-    members = vk_api.get_members(GROUP_ID, 'bdate')
-    logger.info('count: %s' % members.get('count', 0))
-    member_list = members.get('items', [])
-    for member in member_list:
-        r_age[get_age_group(member.get('bdate'))] += 1
+    end_date = date(2015, 4, 1)
+    vk_api = Vk()
 
-    # интересы
-    members = vk_api.get_members(GROUP_ID, 'interests')
-    logger.info('count: %s' % members.get('count', 0))
-    member_list = members.get('items', [])
-    p1 = re.compile("[\u0400-\u0500a-z\s\'\"]{4,}")
-    for member in member_list:
-        inter = member.get('interests', False)
-        if inter:
-            user_i = p1.findall(inter.lower())
-            user_i = [i.strip() for i in user_i]
-            user_i = list(set(user_i))
-            r_interests += user_i
+    # пол
+    for i in xrange(0, 1500, 500):
+        members = vk_api.get_members(GROUP_ID, 'sex', 500, i)
+        logger.info('count: %s' % members.get('count', 0))
+        member_list = members.get('items', [])
+        for member in member_list:
+            gender = int(member.get('sex', 0))
+            if gender == 1:
+                r_gender['female'] += 1
+            if gender == 2:
+                r_gender['male'] += 1
+            if gender == 0:
+                r_gender['?'] += 1
+
+        # возраст
+        members = vk_api.get_members(GROUP_ID, 'bdate', 500, i)
+        logger.info('count: %s' % members.get('count', 0))
+        member_list = members.get('items', [])
+        for member in member_list:
+            r_age[get_age_group(member.get('bdate'))] += 1
+
+        # интересы
+        members = vk_api.get_members(GROUP_ID, 'interests', 500, i)
+        logger.info('count: %s' % members.get('count', 0))
+        member_list = members.get('items', [])
+        p1 = re.compile("[\u0400-\u0500a-z\s\'\"]{4,}")
+        for member in member_list:
+            inter = member.get('interests', False)
+            if inter:
+                user_i = p1.findall(inter.lower())
+                user_i = [i.strip() for i in user_i]
+                user_i = list(set(user_i))
+                r_interests += user_i
 
     # результат
     with open('lab5statistics.json', 'w') as f:
--- lab_5/vk_lib.py
+++ lab_5/vk_lib.py
@@ -10,8 +10,9 @@ class Vk(object):
     def __init__(self):
         self._url = 'http://ec2-52-17-77-210.eu-west-1.compute.amazonaws.com/method/'
 
-    def get_members(self, group_id, field):
+    def get_members(self, group_id, field, count, offset):
         method = 'groups.getMembers'
         _fields = [field]
-        r = requests.get(self._url + method, params={'group_id': group_id, 'fields': _fields})
+        _params = {'group_id': group_id, 'fields': _fields, 'count': count, 'offset': offset}
+        r = requests.get(self._url + method, params=_params)
         return r.json().get('response', {})
