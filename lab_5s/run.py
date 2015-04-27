07b78f19038500a5836141df3556187e109af6ee
Pavel Gladkov <p.gladkov@vectordigital.ru>
2015-04-27 15:56:24 +0300

    lab 5s

--- lab_5s/run.py
+++ lab_5s/run.py
@@ -6,10 +6,19 @@ import cPickle
 import json
 import Queue
 import time
-
+import logging
 import networkx as nx
-
 import requests
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%m-%d-%y %H:%M:%S')
+
+logging.getLogger('urllib3.connectionpool').setLevel(logging.ERROR)
+
+logger = logging.getLogger(__name__)
+
 
 MY_GROUP_ID = 29937606
 MY_USER_ID = 144597051
@@ -48,25 +57,26 @@ pages = (
 )
 
 
-IDS_DUMP_FILENAME = "all_ids.pickle"
+def get_user_ids():
+    ids_dump_filename = "all_ids.pickle"
+    if not os.path.exists(ids_dump_filename):
+        user_ids = set()
+        for i, page in enumerate(pages):
+            logger.info("Page {0} from {1}".format(i, int(float(COUNT) / WINDOW_SIZE)))
+            for user_id in page:
+                user_ids.add(user_id)
 
-if not os.path.exists(IDS_DUMP_FILENAME):
-    USER_IDS = set()
-    for i, page in enumerate(pages):
-        print(
-            "Page {0} from {1}".format(i, int(float(COUNT) / WINDOW_SIZE))
-        )
-        for user_id in page:
-            USER_IDS.add(user_id)
-
-    with open(IDS_DUMP_FILENAME, "wb") as dump:
-        cPickle.dump(USER_IDS, dump, cPickle.HIGHEST_PROTOCOL)
-else:
-    with open(IDS_DUMP_FILENAME, "rb") as dump:
-        USER_IDS = cPickle.load(dump)
+        with open(ids_dump_filename, "wb") as _dump:
+            cPickle.dump(user_ids, _dump, cPickle.HIGHEST_PROTOCOL)
+    else:
+        with open(ids_dump_filename, "rb") as _dump:
+            user_ids = cPickle.load(_dump)
+    return user_ids
 
 GRAPH_DUMP_FILENAME = "graph.pickle"
+USER_IDS = get_user_ids()
 
+# строим граф
 if not os.path.exists(GRAPH_DUMP_FILENAME):
     graph = nx.Graph()
 
@@ -86,13 +96,8 @@ if not os.path.exists(GRAPH_DUMP_FILENAME):
         friends_ids = set(requests.get(
             build_friends_request(current_id)
         ).json()['response']['items']) & USER_IDS - visited
-        print(
-            "Found friend N{0}: {1} ({2} friends)".format(
-                counter,
-                current_id,
-                len(friends_ids)
-            )
-        )
+
+        logger.info("Found friend N{0}: {1} ({2} friends)".format(counter, current_id, len(friends_ids)))
 
         for f_id in friends_ids:
             graph.add_node(f_id)
